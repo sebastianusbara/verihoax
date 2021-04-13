9358c1bae4c0369f3ccc727a4faf1dccc91061d3
Sebastianus Bara Primananda <sebastianus.primananda@btpn.com>
2021-04-13 12:55:31 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import logo from './logo.svg';
+import {
+    Button, Input, Jumbotron, InputGroup, Card, CardImg, CardBody, CardTitle, CardSubtitle, CardText,
+    Navbar, Nav, NavItem
+} from 'reactstrap';
+import { useMediaQuery } from 'react-responsive';
+
+const navItem = {
+    padding: '12px',
+    display: 'flex',
+    fontWeight: 500
+};
+
+const Navigation = (
+    <div style={{
+        display: 'flex',
+        justifyContent: 'center'
+    }}>
+        <a href="" style={navItem}>
+            CARA KERJA
+        </a>
+        <a href="" style={navItem}>
+            TENTANG KAMI
+        </a>
+        <a href="" style={navItem}>
+            DUKUNG KAMI
+        </a>
+        <a href="" style={navItem}>
+            CONTACT
+        </a>
+    </div>
+);
+
+function App() {
+  const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
+  const contentPadding = isMobile ? '0' : '32px 128px';
+
+  return (
+      <div>
+          <Jumbotron style={{background: 'white', paddingBottom: '0px'}}>
+              <div style={{ textAlign: 'center', padding: contentPadding }}>
+                  <div style={{
+                      position : 'relative'
+                  }}>
+                    <img src='/logo-verihoax.png' style={{width: '140px'}}/>
+                    <span style={{
+                        position: 'absolute',
+                        bottom: '12px',
+                        marginLeft: '-5px',
+                        background: 'orange',
+                        color: 'white',
+                        padding: '3px 7px',
+                        fontSize: '12px',
+                        borderRadius: '7px',
+                        fontWeight: 'bold'
+                    }}>BETA</span>
+                  </div>
+                  <div style={{
+                      fontSize: isMobile ? '15px' : '21px',
+                      padding: '12px 21px',
+                      marginBottom: '12px',
+                      fontWeight: '500',
+                      color: 'grey'
+                  }}>
+                      Masukkan Link Berita, Pesan Whatsapp, No Telp, atau No Rekening
+                  </div>
+                  <InputGroup size="md">
+                      <Input type="textarea" rows="7" name="text" style={{
+                          borderRadius: '21px',
+                          padding: '12px'
+                      }}/>
+                  </InputGroup>
+                  <Button color="primary" style={{
+                      fontSize: isMobile ? '18px' : '21px',
+                      marginTop: '21px',
+                      padding: '12px 42px',
+                      fontWeight: 500,
+                      width: isMobile ? '100%' : 'auto'
+                  }}>
+                      Verifikasi Sumber Berita
+                  </Button>
+              </div>
+          </Jumbotron>
+          {!isMobile && Navigation}
+      </div>
+  );
+}
+
+export default App;
---
+++ src/Result.js
@@ -0,0 +1,36 @@
+import {Button, Card, CardBody, CardImg, CardSubtitle, CardText, CardTitle} from "reactstrap";
+
+<div style={{
+    margin: '0px 42px'
+}}>
+    <h2>
+        Referensi
+    </h2>
+    <Card>
+        <CardImg style={{width: '160px'}} src="/logo-liputan-6.png" alt="Card image cap" />
+        <CardBody>
+            <CardTitle tag="h5">Cek Fakta: Hoaks Mayora Bagikan Hadiah untuk Rayakan Hari Jadi Ke-50 Tahun</CardTitle>
+            <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
+            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+            <Button>Cek Fakta ke Halaman Sumber</Button>
+        </CardBody>
+    </Card>
+    <Card>
+        <CardImg style={{width: '160px'}} src="/logo-liputan-6.png" alt="Card image cap" />
+        <CardBody>
+            <CardTitle tag="h5">Cek Fakta: Hoaks Mayora Bagikan Hadiah untuk Rayakan Hari Jadi Ke-50 Tahun</CardTitle>
+            <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
+            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+            <Button>Cek Fakta ke Halaman Sumber</Button>
+        </CardBody>
+    </Card>
+    <Card>
+        <CardImg style={{width: '160px'}} src="/logo-liputan-6.png" alt="Card image cap" />
+        <CardBody>
+            <CardTitle tag="h5">Cek Fakta: Hoaks Mayora Bagikan Hadiah untuk Rayakan Hari Jadi Ke-50 Tahun</CardTitle>
+            <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
+            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+            <Button>Cek Fakta ke Halaman Sumber</Button>
+        </CardBody>
+    </Card>
+</div>
